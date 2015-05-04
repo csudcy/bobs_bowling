b08fb79490724c4a014714728529c0c8212afb97
Nicholas Lee <csudcy@gmail.com>
2015-05-04 11:24:12 +0100

    V1 - Added learnings, reset for v2 with some framework changes (added Frame etc.)

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Notes:
 * You need `nose2` to run the tests
 * Run `python game.py` to play a random game
 * Run `python tests.py` to run the unit tests
+
+Learnings:
+* V1:
+** Method names inside TestCase's have to be globally uniqe
+** Game is too big for 30 minutes; concentrate on Frame first next time
--- game.py
+++ game.py
@@ -2,76 +2,47 @@ import random
 
 class Frame(object):
     def __init__(self):
-        #self._number = number
-        self._balls_remaining = 2
-        self._pins_remaining = 10
-        self._score = 0
+        pass
 
     def roll(self, pins):
-        if self.finished:
-            raise Exception('There are no more balls in this frame!')
-        if pins < 0:
-            raise ValueError('Pins cannot be negative!')
-        if pins > self._pins_remaining:
-            raise ValueError('Cannot roll more than the pins remaining in this frame!')
-        self._balls_remaining -= 1
-        self._pins_remaining -= pins
-        self._score += pins
+        raise NotImplementedError()
 
     @property
     def pins_remaining(self):
-        # UNTESTED
-        return self._pins_remaining
+        raise NotImplementedError()
 
     @property
     def score(self):
-        return self._score
+        raise NotImplementedError()
 
     @property
     def finished(self):
-        return self._balls_remaining <= 0
+        raise NotImplementedError()
 
 class Game(object):
     def __init__(self):
-        self._frames = [
-            Frame()
-            for i in xrange(10)
-        ]
-        self._current_frame = 0
+        pass
 
     # Requirements from Bob
     def roll(self, pins):
-        if self.finished:
-            raise RuntimeError('The game is over!')
-        frame = self._frames[self._current_frame]
-        frame.roll(pins)
-        if frame.finished:
-            self._current_frame += 1
+        raise NotImplementedError()
 
     @property
     def score(self):
-        return sum([
-            frame.score
-            for frame in self._frames
-        ])
+        raise NotImplementedError()
 
     # Things that would be really useful
     @property
     def pins_remaining(self):
-        # UNTESTED
-        if self.finished:
-            return 'Game over!'
-        frame = self._frames[self._current_frame]
-        return frame.pins_remaining
+        raise NotImplementedError()
 
     @property
     def frame(self):
-        # UNTESTED
-        return self._current_frame
+        raise NotImplementedError()
 
     @property
     def finished(self):
-        return self._current_frame >= len(self._frames)
+        raise NotImplementedError()
 
 if __name__ == '__main__':
     game = Game()
