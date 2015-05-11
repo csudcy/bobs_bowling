7bc5bfd7e0591156ec5682b002b6f03209a0870f
Nicholas Lee <csudcy@gmail.com>
2015-05-11 09:46:54 +0100

    V2 - Forgot to save...

--- game.py
+++ game.py
@@ -3,55 +3,24 @@ import random
 
 class Frame(object):
     def __init__(self):
-        self._pins_remaining = 10
-        self._rolls_total = 2
-        self._rolls_remaining = self._rolls_total
-        self._rolls = []
+        raise NotImplementedError()
 
     def roll(self, pins):
-        if self.finished:
-            raise Exception('You have no rolls remaining in this frame!')
-        if pins < 0:
-            raise ValueError('You must roll a positive number of pins!')
-        if pins > self._pins_remaining:
-            raise ValueError(
-                'You cannot roll {pins} pin(s) as there are only {pins_remaining} pin(s) left!'.format(
-                    pins=pins,
-                    pins_remaining=self._pins_remaining,
-                )
-            )
-        self._rolls.append(pins)
-        self._pins_remaining -= pins
-        self._rolls_remaining -= 1
+        raise NotImplementedError()
 
     @property
     def pins_remaining(self):
-        # TODO: UNTESTED
-        return self._pins_remaining
+        raise NotImplementedError()
 
     def score(self, next_frame=None, max_rolls=None):
-        # TODO: UNTESTED max_rolls
-        if max_rolls:
-            score = sum(self._rolls[:max_rolls])
-        else:
-            score = sum(self._rolls)
-
-        if score == 10 and next_frame is not None:
-            # There was a spare or a strike, get the next frame's score
-            score += self._get_next_frame_score(next_frame)
-        return score
+        raise NotImplementedError()
 
     def _get_next_frame_score(self, next_frame):
-        # TODO: UNTESTED
-        max_rolls = 1
-        if len(self._rolls) == 1:
-            # This was a strike
-            max_rolls = 2
-        return next_frame.score(max_rolls=max_rolls)
+        raise NotImplementedError()
 
     @property
     def finished(self):
-        return (self._pins_remaining == 0) or (self._rolls_remaining == 0)
+        raise NotImplementedError()
 
 
 class Game(object):
